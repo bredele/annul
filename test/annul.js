2a7d3281c3fdec9f8d0ac0bf8f2b0d28ffd95f15
bredele <olivier.wietrich@gmail.com>
2018-03-03 14:24:00 -0700

    Initialize package

---
+++ test/annul.js
@@ -0,0 +1,13 @@
+/**
+ * Dependencie(s)
+ */
+
+const test = require('tape')
+const promise = require('atleast')
+const cancel = require('..')
+
+
+test('should reject a promise if not resolved before a given time', assert => {
+  assert.plan(1)
+  
+})
