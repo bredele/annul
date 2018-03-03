5a0c43ceab4e8060d8fb7677e15fabd86d3b65c1
bredele <olivier.wietrich@gmail.com>
2018-03-03 14:27:29 -0700

    Return a promise

---
+++ index.js
@@ -0,0 +1,7 @@
+
+
+module.exports = (promise, ms) => {
+  return new Promise((resolve, reject) => {
+
+  })
+}
--- test/annul.js
+++ test/annul.js
@@ -7,7 +7,16 @@ const promise = require('atleast')
 const cancel = require('..')
 
 
-test('should reject a promise if not resolved before a given time', assert => {
-  assert.plan(1)
-  
+test('should return a promise', assert => {
+  assert.plan(2)
+  const annul = cancel(promise('hello', 2000), 200)
+  assert.equal(typeof annul, 'object')
+  assert.equal(typeof annul.then, 'function')
 })
+
+//
+// test('should reject a promise if not resolved before a given time', assert => {
+//   assert.plan(1)
+//   cancel(promise('hello', 2000), 200)
+//     .then(null, () => assert.ok())
+// })
